cd6ce5346f3f36d6914912202ec06a7e4c650cee
Daniel Harper <djharperuk@gmail.com>
2012-02-14 20:35:04 +0000

    Update src/testproject/server.clj

--- src/testproject/server.clj
+++ src/testproject/server.clj
@@ -7,5 +7,5 @@
   (let [mode (keyword (or (first m) :dev))
         port (Integer. (get (System/getenv) "PORT" "8080"))]
     (server/start port {:mode mode
-                        :ns 'tproject})))
+                        :ns 'testproject})))
 
